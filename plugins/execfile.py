91b2ee539342a797d87cb746abfd7cf0215175ae
nufeng1999 <18478162@qq.com>
2021-12-15 21:51:36 +0800

    fix plugins

--- plugins/execfile.py
+++ plugins/execfile.py
@@ -1,32 +1,31 @@
 from typing import Dict, Tuple, Sequence,List
 from plugins.ISpecialID import IStag,IDtag,IBtag,ITag
 import os
-class MyNoruncode(IBtag):
+class Myexecfile(IStag):
     kobj=None
     def getName(self) -> str:
         # self.kobj._write_to_stdout("setKernelobj setKernelobj setKernelobj\n")
-        return 'MyNoruncode'
+        return 'Myexecfile'
     def getAuthor(self) -> str:
         return 'Author'
     def getIntroduction(self) -> str:
-        return 'MyNoruncode'
+        return 'Myexecfile'
     def getPriority(self)->int:
         return 0
     def getExcludeID(self)->List[str]:
         return []
-    def getIDBptag(self) -> List[str]:
-        return ['noruncode']
+    def getIDSptag(self) -> List[str]:
+        return ['execfile','binfile']
     def setKernelobj(self,obj):
         self.kobj=obj
         # self.kobj._write_to_stdout("setKernelobj setKernelobj setKernelobj\n")
         return
     def on_shutdown(self, restart):
         return
-    def on_IBpCodescanning(self,magics,line) -> str:
+    def on_ISpCodescanning(self,key, value,magics,line) -> str:
         # self.kobj._write_to_stdout(line+" on_ISpCodescanning\n")
-        self.kobj.addkey2dict(magics,'noruncode')
-        magics['noruncode'] = ['true']
-        return ''
+        self.kobj.addkey2dict(magics,'execfile')
+        return self.commandhander(self,key, value,magics,line)
     def on_Codescanning(self,magics,code)->Tuple[bool,str]:
         pass
         return False,code
@@ -44,3 +43,7 @@ class MyNoruncode(IBtag):
         return False
     def on_after_completion(self,returncode,execfile,magics)->bool:
         return False
+    def commandhander(self,key, value,magics,line):
+        # self.kobj._write_to_stdout(value+"  exec kcmd...\n")
+        magics['execfile'] = value
+        return ''
