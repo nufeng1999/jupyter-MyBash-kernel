0482b95d3d4bae4d9e0759c0f71f6258b5c8e6fa
nufeng1999 <18478162@qq.com>
2021-12-15 09:26:10 +0800

    Init project

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='jupyter_MyBash_kernel',
+      version='0.0.1',
+      description='Minimalistic Bash kernel for Jupyter',
+      author='nufeng',
+      author_email='18478162@qq.com',
+      license='MIT',
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+      ],
+      url='https://github.com/nufeng1999/jupyter-MyBash-kernel/',
+      download_url='https://github.com/nufeng1999/jupyter-MyBash-kernel/tarball/0.0.1',
+      packages=['jupyter_MyBash_kernel'],
+      scripts=['jupyter_MyBash_kernel/install_MyBash_kernel'],
+      keywords=['jupyter', 'notebook', 'kernel', 'bash','shell','sh'],
+      include_package_data=True
+      )
