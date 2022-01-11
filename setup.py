d6e5efec98e72a2fc6aceaefd5b2b8384ca6634a
nufeng1999 <18478162@21cn.com>
2022-01-11 17:17:01 +0800

    move plugins

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
-from setuptools import setup
-
-setup(name='jupyter_MyBash_kernel',
-      version='0.0.1',
-      description='Minimalistic shellscript kernel for Jupyter',
-      author='nufeng',
-      author_email='18478162@qq.com',
-      license='MIT',
-      classifiers=[
-          'License :: OSI Approved :: MIT License',
-      ],
-      url='https://github.com/nufeng1999/jupyter-MyBash-kernel/',
-      download_url='https://github.com/nufeng1999/jupyter-MyBash-kernel/tarball/0.0.1',
-      packages=['jupyter_MyBash_kernel'],
-      scripts=['jupyter_MyBash_kernel/install_MyBash_kernel'],
-      keywords=['jupyter', 'notebook', 'kernel', 'shell','shellscript','Bash','sh'],
-      include_package_data=True
-      )
+#!/usr/bin/env python
+# coding: utf-8
+with open("README.md", "r") as f:
+	long_description = f.read()
+import setuptools
+setuptools.setup(name='jupyter_MyBash_kernel',
+    version='0.0.1',
+    description='Minimalistic shellscript kernel for Jupyter',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='nufeng',
+    author_email='18478162@qq.com',
+    license='MIT',
+    url='https://github.com/nufeng1999/jupyter-MyBash-kernel/',
+    download_url='https://github.com/nufeng1999/jupyter-MyBash-kernel/releases/tag/0.0.1',
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ],
+    scripts=['jupyter_MyBash_kernel/install_MyBash_kernel'],
+    keywords=['jupyter', 'notebook', 'kernel', 'shell','shellscript','Bash','sh'],
+    include_package_data=True
+    )
